28c73bc2bbcbf3be030e452424ea83d8ea1bfbcc
Shubham <schhabra687@gmail.com>
2016-03-15 01:36:31 +0530

    jframe code

---
+++ jframe.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+class myframe
+{
+	public static void main(String arg[])
+	{
+		JFrame j= new JFrame("New Frame");
+		j.setSize(600,450);
+		j.setLayout(null);
+		j.setVisible(true);
+	}
+}
